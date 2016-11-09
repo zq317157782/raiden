4c3851f11536130d8b3d8c5e68f66b0c22a0f959
诸谦 <zq317157782@163.com>
2016-11-09 14:39:14 +0800

    add [raiden.h]
    add global header [raiden.h];every other core class need import it

---
+++ src/core/raiden.h
@@ -0,0 +1,16 @@
+/*
+ * raiden.h
+ *
+ *  Created on: 2016年11月9日
+ *      Author: zhuqian
+ */
+
+#ifndef SRC_CORE_RAIDEN_H_
+#define SRC_CORE_RAIDEN_H_
+//raiden.h包含所有全局设置
+//radien.h includes all global setting
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#endif /* SRC_CORE_RAIDEN_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
  *  Created on: 2016年11月9日
  *      Author: zhuqian
  */
-#include <iostream>
+#include "raiden.h"
 int main(){
 	std::cout<<"hello raiden!!"<<std::endl;
 }
