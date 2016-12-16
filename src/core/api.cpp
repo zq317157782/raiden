c6d5bc86dd7ef5efc2b28d40d2e57272e340134b
诸谦 <zq317157782@163.com>
2016-12-16 13:08:25 +0800

    add RenderOptions

--- src/core/api.cpp
+++ src/core/api.cpp
@@ -6,6 +6,7 @@
  */
 #include "api.h"
 #include "transform.h"
+#include "paramset.h"
 //transform相关参数
 constexpr int MaxTransforms = 2;
 constexpr int StartTransformBits = 1 << 0;	//0x01
@@ -25,21 +26,47 @@ public:
 		return _t[i];
 	}
 	//判断是否包含不同的transform
-	bool IsAnimated() const{
-		for(int i=0;i<MaxTransforms-1;++i){
-			if(_t[i]!=_t[i+1]){
+	bool IsAnimated() const {
+		for (int i = 0; i < MaxTransforms - 1; ++i) {
+			if (_t[i] != _t[i + 1]) {
 				return true;
 			}
 		}
 		return false;
 	}
 	//返回transformSet的逆
-	friend TransformSet Inverse(const TransformSet& ts){
+	friend TransformSet Inverse(const TransformSet& ts) {
 		TransformSet invSet;
-		for(int i=0;i<MaxTransforms;++i){
-			invSet._t[i]=Inverse(ts._t[i]);
+		for (int i = 0; i < MaxTransforms; ++i) {
+			invSet._t[i] = Inverse(ts._t[i]);
 		}
 		return invSet;
 	}
 };
-
+//渲染参数
+struct RenderOptions {
+	Float transformStartTime = 0;
+	Float transformEndTime = 1;
+	//过滤器名字  默认 box
+	std::string FilterName = "box";
+	ParamSet FilterParams;
+	//Film的名字
+	std::string FilmName = "image";
+	ParamSet FilmParams;
+	//采样器的名字
+	std::string SamplerName = "random";
+	ParamSet SamplerParams;
+	//加速结构的名字
+	std::string AcceleratorName = "normal";
+	ParamSet AcceleratorParams;
+	//积分器的名字
+	std::string IntegratorName = "normal";
+	ParamSet IntegratorParams;
+	//相机的名字
+	std::string CameraName = "pinhole";
+	ParamSet CameraParams;
+	//光源
+	std::vector<std::shared_ptr<Light>> lights;
+	//图元
+	std::vector<std::shared_ptr<Primitive>> primitives;
+};
