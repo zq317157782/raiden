778cad31be9919096b1eac0b89a76dc353635f06
诸谦 <zq317157782@163.com>
2017-01-05 14:22:20 +0800

    add distant.cpp/h

---
+++ src/lights/distant.cpp
@@ -0,0 +1,10 @@
+/*
+ * distant.cpp
+ *
+ *  Created on: 2017年1月5日
+ *      Author: zhuqian
+ */
+
+
+
+
---
+++ src/lights/distant.h
@@ -0,0 +1,16 @@
+/*
+ * distant.h
+ *
+ *  Created on: 2017年1月5日
+ *      Author: zhuqian
+ */
+
+#ifndef SRC_LIGHTS_DISTANT_H_
+#define SRC_LIGHTS_DISTANT_H_
+#include "raiden.h"
+#include "light.h"
+
+
+
+
+#endif /* SRC_LIGHTS_DISTANT_H_ */
