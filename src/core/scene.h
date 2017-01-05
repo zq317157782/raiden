710a23d8406412a003f7613c2417420fe4944608
诸谦 <zq317157782@163.com>
2017-01-05 13:52:28 +0800

    add WorldBound to Scene

--- src/core/scene.h
+++ src/core/scene.h
@@ -13,14 +13,20 @@
 class Scene{
 private:
 	std::shared_ptr<Primitive> _aggregate;//图元集合
+	Bound3f _worldBound;
 public:
 	const std::vector<std::shared_ptr<Light>> lights;
 public:
 	Scene(const std::shared_ptr<Primitive>&aggregate,const std::vector<std::shared_ptr<Light>>&lights):_aggregate(aggregate),lights(lights){
 		Assert(_aggregate!=nullptr);
+		_worldBound=_aggregate->WorldBound();//获得整个scene的world边界
 	}
 	bool Intersect(const Ray& ray,SurfaceInteraction* interaction) const;
 	bool IntersectP(const Ray& ray) const;
+
+	const Bound3f& WorldBound() const{
+		return _worldBound;
+	}
 };
 
 
