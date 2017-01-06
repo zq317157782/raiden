c354ffe43d587626772e95d7c7a0f86142919d72
zhuqian <zhuqian@fantablade.com>
2017-01-06 16:31:29 +0800

    add TriangleMesh

--- src/shapes/trianglemesh.cpp
+++ src/shapes/trianglemesh.cpp
@@ -4,7 +4,37 @@
  *  Created on: 2017年1月6日
  *      Author: zhuqian
  */
+#include "trianglemesh.h"
 
+TriangleMesh::TriangleMesh(const Transform& ObjectToWorld, int nTriangles, const int* vertexIndices,
+	int nVertices, const Point3f* P, const Vector3f *S, const Normal3f *N, const Point2f *UV):numTriangles(nTriangles), numVertices(nVertices),vertexIndices(vertexIndices, vertexIndices+3*numTriangles){
+	
+	vertices.reset(new Point3f[numVertices]);
+	//转换到世界坐标系下
+	for (int i = 0; i < numVertices; ++i) {
+		vertices[i] = ObjectToWorld(P[i]);
+	}
 
+	//法线
+	if (N) {
+		normals.reset(new Normal3f[numVertices]);
+		for (int i = 0; i < numVertices; ++i) {
+			normals[i] = ObjectToWorld(N[i]);
+		}
+	}
 
+	//切线
+	if (S) {
+		tangents.reset(new Vector3f[numVertices]);
+		for (int i = 0; i < numVertices; i++)
+		{
+			tangents[i] = ObjectToWorld(S[i]);
+		}
+	}
+
+	if (UV) {
+		uv.reset(new Point2f[numVertices]);
+		memcpy(uv.get(), UV, numVertices * sizeof(Point2f));
+	}
+}
 
--- src/shapes/trianglemesh.h
+++ src/shapes/trianglemesh.h
@@ -7,8 +7,22 @@
 
 #ifndef SRC_SHAPES_TRIANGLEMESH_H_
 #define SRC_SHAPES_TRIANGLEMESH_H_
+#include "raiden.h"
+#include "shape.h"
 
+//三角Mesh
+struct TriangleMesh {
+	const int numTriangles;//三角面片的个数
+	const int numVertices; //顶点个数
+	std::vector<int> vertexIndices;//索引
+	std::unique_ptr<Point3f[]> vertices;//顶点数据
+	std::unique_ptr<Normal3f[]> normals; //逐顶点法线
+	std::unique_ptr<Vector3f[]> tangents; //切线
+	std::unique_ptr<Point2f[]> uv; //纹理坐标
 
+	TriangleMesh(const Transform& ObjectToWorld, int nTriangles/*三角面片个数*/,const int* vertexIndices/*顶点索引*/, 
+		int nVertices/*顶点个数*/, const Point3f* P/*顶点数组*/, const Vector3f *S/*切线数组*/, const Normal3f *N/*法线数组*/, const Point2f *uv/*纹理坐标数组*/);
+};
 
 
 
