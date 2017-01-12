bb311d979c353e2998eb2d892f2b5b0534cab0a3
诸谦 <zq317157782@163.com>
2017-01-12 22:55:49 +0800

    fix some grid bug

--- src/accelerators/grid.h
+++ src/accelerators/grid.h
@@ -43,15 +43,16 @@ class Grid: public Aggregate {
 private:
 	std::vector<std::shared_ptr<Primitive>> _primitives;
 	Bound3f _worldBound;
-	Float _voxelPerUnit; //每个空间单位下体素个数
-	Float _widths[3];
+	Float _widths[3]; //宽度每体素
+	Float _invWidths[3]; //单位宽度下的体素个数
 	int _voxelNum[3]; //每个轴下的体素个数
 	int _totalVoxelNum;
 	Voxel** _voxels; //体素集合
 	MemoryArena _voxelArena;
 private:
 	int PosToVoxel(const Point3f& p, int axis) const {
-		int ret = (int) (p[axis] - _worldBound.minPoint[axis]) * _voxelPerUnit;
+		int ret = (int) (p[axis] - _worldBound.minPoint[axis])
+				* _invWidths[axis];
 		return Clamp(ret, 0, _voxelNum[axis] - 1);
 	}
 
@@ -74,13 +75,16 @@ public:
 
 		Vector3f diagonal = _worldBound.Diagonal();
 		int maxAxis = MaxDimension(diagonal);
-		_voxelPerUnit = (std::pow(_primitives.size(), 1.0f / 3.0f)*3) / diagonal[maxAxis];
+		Float voxelPerUnit = (std::pow(_primitives.size(), 1.0f / 3.0f) * 3)
+				/ diagonal[maxAxis];
 
 		//计算每个轴下的体素个数
 		for (int axis = 0; axis < 3; ++axis) {
-			_voxelNum[axis] = (int)std::round(_voxelPerUnit * diagonal[axis]);
+			_voxelNum[axis] = (int) std::round(voxelPerUnit * diagonal[axis]);
 			_voxelNum[axis] = Clamp(_voxelNum[axis], 1, 64); //裁剪在1~64的范围内
-			_widths[axis]=diagonal[axis]/_voxelNum[axis];
+			_widths[axis] = diagonal[axis] / _voxelNum[axis];
+			_invWidths[axis] =
+					(_widths[axis] == 0) ? (0) : (1.0f / _widths[axis]);
 		}
 
 		//总共的体素个数
@@ -98,24 +102,18 @@ public:
 				maxV[j] = PosToVoxel(b.maxPoint, j);
 			}
 
-			for (int x = minV[0]; x <= maxV[0]; ++x) {
+			for (int z = minV[2]; z <= maxV[2]; ++z) {
 				for (int y = minV[1]; y <= maxV[1]; ++y) {
-					for (int z = minV[2]; z <= maxV[2]; ++z) {
+					for (int x = minV[0]; x <= maxV[0]; ++x) {
 						int o = Offset(x, y, z);
 						if (_voxels[o] == nullptr) {
 							_voxels[o] = _voxelArena.Alloc<Voxel>();
 						}
 						(*_voxels[o]).AddPrimitive(_primitives[i]);
-
 					}
 				}
 			}
 		}
-//		for(int i=0;i<_totalVoxelNum;++i){
-//			if(_voxels[i]){
-//				Debug("id:"<<i<<" num:"<<_voxels[i]->primitives.size());
-//			}
-//		}
 	}
 	~Grid() {
 		_voxelArena.Reset();
@@ -133,49 +131,52 @@ public:
 
 		Float nextCrossingT[3];
 		Float deltaT[3];
-		int step[3],out[3],pos[3];
+		int step[3], out[3], pos[3];
 
 		//初始化
-		for(int axis=0;axis<3;++axis){
-			pos[axis]=PosToVoxel(hitPoint,axis);//获得当前voxel的坐标成分
-			if(r.d[axis]>=0){
+		for (int axis = 0; axis < 3; ++axis) {
+			pos[axis] = PosToVoxel(hitPoint, axis); //获得当前voxel的坐标成分
+			if (r.d[axis] >= 0) {
 				//计算向正方向偏移到下个voxel和voxel相交处的射线参数
-				nextCrossingT[axis]=hit+(VoxelToPos(pos[axis]+1,axis)-hitPoint[axis])/r.d[axis];
-				step[axis]=1;//向正方向的标记
-				out[axis]=_voxelNum[axis];//到第几个voxel结束
-				deltaT[axis]=_widths[axis]/r.d[axis];
-			}
-			else{
-				nextCrossingT[axis]=hit+(VoxelToPos(pos[axis],axis)-hitPoint[axis])/r.d[axis];
-				step[axis]=1;//向负方向的标记
-				out[axis]=-1;
-				deltaT[axis]=-_widths[axis]/r.d[axis];
+				nextCrossingT[axis] = hit
+						+ (VoxelToPos(pos[axis] + 1, axis) - hitPoint[axis])
+								/ r.d[axis];
+				step[axis] = 1; //向正方向的标记
+				out[axis] = _voxelNum[axis]; //到第几个voxel结束
+				deltaT[axis] = _widths[axis] / r.d[axis];
+			} else {
+				nextCrossingT[axis] = hit
+						+ (VoxelToPos(pos[axis], axis) - hitPoint[axis])
+								/ r.d[axis];
+				step[axis] = 1; //向负方向的标记
+				out[axis] = -1;
+				deltaT[axis] = -_widths[axis] / r.d[axis];
 			}
 		}
 
-		bool isHit=false;
-		for(;;){
+		bool isHit = false;
+		for (;;) {
 			//获取当前voxel
-			Voxel* voxel=_voxels[Offset(pos[0],pos[1],pos[2])];
-			if(voxel){
-				isHit|=voxel->Intersect(r,ref);
+			Voxel* voxel = _voxels[Offset(pos[0], pos[1], pos[2])];
+			if (voxel) {
+				isHit |= voxel->Intersect(r, ref);
 				int axis;
-				if(nextCrossingT[0]<nextCrossingT[1]&&nextCrossingT[0]<nextCrossingT[2]){
-					axis=0;
-				}else if(nextCrossingT[1]<nextCrossingT[2]){
-					axis=1;
+				if (nextCrossingT[0] < nextCrossingT[1]
+						&& nextCrossingT[0] < nextCrossingT[2]) {
+					axis = 0;
+				} else if (nextCrossingT[1] < nextCrossingT[2]) {
+					axis = 1;
+				} else {
+					axis = 2;
 				}
-				else{
-					axis=2;
-				}
-				if(r.tMax<nextCrossingT[axis]){
+				if (r.tMax < nextCrossingT[axis]) {
 					break;
 				}
-				pos[axis]+=step[axis];//更新位置
-				if(pos[axis]==out[axis]){
+				pos[axis] += step[axis]; //更新位置
+				if (pos[axis] == out[axis]) {
 					break;
 				}
-				nextCrossingT[axis]+=deltaT[axis];
+				nextCrossingT[axis] += deltaT[axis];
 			}
 			return isHit;
 		}
@@ -231,9 +232,69 @@ public:
 		return _worldBound;
 	}
 	bool IntersectP(const Ray& r) const override {
-		return false;
+		Float hit;
+		if (Inside(r.o, _worldBound)) {
+			hit = 0;
+		} else if (!_worldBound.IntersectP(r, &hit)) {
+			return false;
+		}
+		Point3f hitPoint = r(hit); //和grid的相交点
+
+		Float nextCrossingT[3];
+		Float deltaT[3];
+		int step[3], out[3], pos[3];
+
+		//初始化
+		for (int axis = 0; axis < 3; ++axis) {
+			pos[axis] = PosToVoxel(hitPoint, axis); //获得当前voxel的坐标成分
+			if (r.d[axis] >= 0) {
+				//计算向正方向偏移到下个voxel和voxel相交处的射线参数
+				nextCrossingT[axis] = hit
+						+ (VoxelToPos(pos[axis] + 1, axis) - hitPoint[axis])
+								/ r.d[axis];
+				step[axis] = 1; //向正方向的标记
+				out[axis] = _voxelNum[axis]; //到第几个voxel结束
+				deltaT[axis] = _widths[axis] / r.d[axis];
+			} else {
+				nextCrossingT[axis] = hit
+						+ (VoxelToPos(pos[axis], axis) - hitPoint[axis])
+								/ r.d[axis];
+				step[axis] = 1; //向负方向的标记
+				out[axis] = -1;
+				deltaT[axis] = -_widths[axis] / r.d[axis];
+			}
+		}
+
+		for (;;) {
+			//获取当前voxel
+			Voxel* voxel = _voxels[Offset(pos[0], pos[1], pos[2])];
+			if (voxel) {
+				if (voxel->IntersectP(r)) {
+					return true;
+				}
+				int axis;
+				if (nextCrossingT[0] < nextCrossingT[1]
+						&& nextCrossingT[0] < nextCrossingT[2]) {
+					axis = 0;
+				} else if (nextCrossingT[1] < nextCrossingT[2]) {
+					axis = 1;
+				} else {
+					axis = 2;
+				}
+				if (r.tMax < nextCrossingT[axis]) {
+					break;
+				}
+				pos[axis] += step[axis]; //更新位置
+				if (pos[axis] == out[axis]) {
+					break;
+				}
+				nextCrossingT[axis] += deltaT[axis];
+			}
+			return false;
+		}
 	}
-};
+}
+;
 
 std::shared_ptr<Grid> CreateGridAccelerator(
 		const std::vector<std::shared_ptr<Primitive>> &prims,
