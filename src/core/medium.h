cbe1dbe9af041766e5d62ec07108f5ade8a6629b
zhuqian <zhuqian@fantablade.com>
2017-01-20 16:12:06 +0800

    add HenyeyGreenstein

--- src/core/medium.h
+++ src/core/medium.h
@@ -16,9 +16,14 @@ class PhaseFunction {
 public:
 	virtual Float P(const Vector3f& wo,const Vector3f& wi)const = 0;
 	virtual ~PhaseFunction() {}
+	//采样PhaseFunction
+	virtual Float Sample_p(const Vector3f &wo, Vector3f *wi,
+		const Point2f &u) const = 0;
 
 	virtual std::string ToString() const = 0;
+
 };
+
 
 
 inline std::ostream &operator<<(std::ostream &os, const PhaseFunction &p) {
@@ -32,6 +37,42 @@ inline Float PhaseHG(Float cosTheta, Float g) {
 	return Inv4Pi * (1 - g * g) / (denom * std::sqrt(denom));
 }
 
+
+//HenyeyGreenstein PhaseFunction
+class HenyeyGreenstein :public PhaseFunction {
+private:
+	Float _g;//控制参数,范围-1~1,正数和负数代表了反射回去还是向前反射
+public:
+	HenyeyGreenstein(Float g) :_g(g) {}
+
+	Float P(const Vector3f& wo, const Vector3f& wi) const override {
+		return PhaseHG(Dot(wo, wi), _g);
+	}
+
+	Float Sample_p(const Vector3f &wo, Vector3f *wi,
+		const Point2f &u) const override {
+		//采样costheta
+		Float cosTheta;
+		if (std::abs(_g) < 1e-3) {
+			//_g为0的时候用均匀采样
+			cosTheta = 1 - 2 * u[0];
+		}
+		else {
+			//取CDF的逆函数
+			Float sqrTerm = (1 - _g * _g) / (1 - _g + 2 * _g * u[0]);
+			cosTheta = (1 + _g * _g - sqrTerm * sqrTerm) / (2 * _g);
+		}
+		//采样入射方向
+		Float sinTheta = std::sqrt(std::max((Float)0, 1 - cosTheta * cosTheta));
+		Float phi = 2 * Pi * u[1];
+		Vector3f v1, v2;
+		CoordinateSystem(wo, &v1, &v2);
+		//-wo取0是因为PBRT的wi方向和学术界论文中常用的方向正好相反
+		*wi = SphericalDirection(sinTheta, cosTheta, phi, v1, v2, -wo);
+		//-cosTheta理由同上
+		return PhaseHG(-cosTheta, _g);
+	}
+};
 
 //材质介质
 class Medium {
