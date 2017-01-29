cf59896ac6506fe4965e03fe6c4c19bd62091b7c
诸谦 <zq317157782@163.com>
2017-01-29 16:33:34 +0800

    add code to show time in progressreporter

--- src/core/progressreporter.h
+++ src/core/progressreporter.h
@@ -18,27 +18,26 @@ private:
 	std::atomic<uint64_t> _doneNum;//已经完成了个工作数
 	uint64_t _totalNum;//工作数总数
 	const std::string _title;//进度条标题
-	const const std::chrono::system_clock::time_point _startTime;
+	const std::chrono::system_clock::time_point _startTime;
 	std::atomic<bool> _threadExit;
 	std::thread _updateThread;
 	void PrintBar(){
 		//线程的睡眠时间
-		std::chrono::milliseconds sleepDuration(250);
-		int totalLength=80;
+		std::chrono::milliseconds sleepDuration(500);
+		int totalLength=88;
 		char buf[totalLength];
+		for(int i=0;i<88;++i){
+			buf[i]=' ';
+		}
 		snprintf(buf,_title.size()+1,_title.c_str());
 		buf[_title.size()]='[';
 		char* barStart=buf+_title.size()+1;
-		for(int i=0;i<50;++i){
-			barStart[i]=' ';
-		}
 		barStart[50]=']';
-		barStart[52]='\n';
-		barStart[53]='\0';
-		uint64_t it=0;
+		barStart[87]='\0';
 		while(!_threadExit){
-			for(int i=0;i<_title.size()+54;++i){
-					printf("\b");
+			//清空上次一的数据
+			for(int i=0;i<89;++i){
+				printf("\b");
 			}
 			std::this_thread::sleep_for(sleepDuration);
 			Float percent=(Float)(_doneNum)/(_totalNum);
@@ -46,14 +45,21 @@ private:
 			for(int i=0;i<num;++i){
 				barStart[i]='+';
 			}
-			fputs(buf, stdout);
-			fflush(stdout);
-			it++;
+
+			//统计剩余时间
+			 std::chrono::system_clock::time_point now =std::chrono::system_clock::now();
+			 long long ep= std::chrono::duration_cast<std::chrono::milliseconds>(now-_startTime).count();
+			 Float seconds=ep/1000.0f;//已经经过的时间
+			 Float nokotaSeconds=seconds/percent-seconds;//剩余的时间
+
+			 snprintf(buf+52+_title.size(),24,"(s:%.1f|es:%.1f)              ",seconds,nokotaSeconds);
+			 fputs(buf, stdout);
+			 fflush(stdout);
 		}
 		printf("\n");
 	}
 public:
-	ProgressReporter(uint64_t totalNum,const std::string& title):_totalNum(std::max((uint64_t)1,totalNum)),_title(title){
+	ProgressReporter(uint64_t totalNum,const std::string& title):_totalNum(std::max((uint64_t)1,totalNum)),_title(title),_startTime(std::chrono::system_clock::now()){
 		_doneNum=0;
 		_threadExit=false;
 		//开启更新进程
@@ -78,6 +84,8 @@ public:
 	//完成进度
 	void Done(){
 		_doneNum=_totalNum;
+
+
 	}
 };
 
