821053bbfe7f51bfbb3ec19c9e1836dcbad11399
诸谦 <zq317157782@163.com>
2017-05-16 21:34:50 +0800

    add some basic vertex funcs

--- src/integrators/bdpt.h
+++ src/integrators/bdpt.h
@@ -13,6 +13,9 @@
 #include "integrator.h"
 #include "camera.h"
 #include "film.h"
+#include "reflection.h"
+#include "light.h"
+#include "primitive.h"
 //代表lens上的或者光源上的一个点
 struct EndpointInteraction: public Interaction {
 public:
@@ -110,6 +113,123 @@ struct Vertex {
 	Vertex &operator=(const Vertex &v) {
 		memcpy(this, &v, sizeof(Vertex));
 		return *this;
+	}
+
+	//获取Interaction的引用
+	const Interaction& GetInteraction() const {
+		//PBRT这里为了可读性，进行了类型的判断，我这里直接去除了
+		return ei;
+	}
+
+	//返回空间点
+	const Point3f& p() const {
+		return GetInteraction().p;
+	}
+
+	//返回几何法线
+	const Normal3f& ng() const {
+		return GetInteraction().n;
+	}
+
+	//返回着色法线,只有Surface有着色法线，其余的直接用几何法线
+	const Normal3f& ns() const {
+		if (type == VertexType::Surface) {
+			return si.shading.n;
+		}
+		return GetInteraction().n;
+	}
+
+	//判断是否在Surface上，用法线来判断，如果没有法线就说明不在Surface上
+	bool IsOnSurface() const {
+		if (GetInteraction().n == Normal3f(0, 0, 0)) {
+			return false;
+		}
+		return true;
+	}
+
+	//计算相应的brdf系数
+	//因为wo已经在interaction中了，所以这里只要提供next节点，以及transport mdoe
+	Spectrum f(const Vertex& next, TransportMode mode) const {
+		//计算wi
+		Vector3f wi = next.p() - p();
+		if (wi.LengthSquared() == 0) {
+			return Spectrum(0);
+		}
+		wi = Normalize(wi);
+		switch (type) {
+		case VertexType::Surface: {
+			//这里要修正shading normal带来的不对称性
+			return si.bsdf->f(si.wo, wi)
+					* CorrectShadingNormal(si, si.wo, wi, mode);
+		}
+			break;
+		case VertexType::Medium: {
+			//中间介质不需要关心shading normal带来的不对称性
+			return mi.phase->P(mi.wo, wi);
+		}
+			break;
+
+		default: {
+			Warning("Unimplement for this type!");
+			return Spectrum(0);
+		}
+		}
+	}
+
+	//判断是否能够连接，其实就是判断是否有Dirac分布
+	bool IsConnectable() const {
+		if (type == VertexType::Camera) {
+			return true;
+		} else if (type == VertexType::Light) {
+			return ei.light->flags & (int) LightFlags::DeltaDirection == 0;
+		} else if (type == VertexType::Medium) {
+			return true;
+		} else if (type == VertexType::Surface) {
+			return si.bsdf->NumComponents(
+					BxDFType(
+							BxDFType::BSDF_REFLECTION
+									| BxDFType::BSDF_TRANSMISSION
+									| BxDFType::BSDF_DIFFUSE
+									| BxDFType::BSDF_GLOSSY)) > 0;
+		}
+		return false;
+	}
+
+	//判断是否是光源
+	//1.type为光源
+	//2.type为Surface 并且是区域光
+	bool IsLight() const {
+		if (type == VertexType::Light
+				|| (type == VertexType::Surface && si.primitive->GetAreaLight())) {
+			return true;
+		}
+		return false;
+	}
+
+	//判断是否是delta光源
+	bool IsDeltaLight() const {
+	if(type==VertexType::Light&&ei.light&&::IsDeltaLight(ei.light->flags)){
+		return true;
+	}
+	return false;
+}
+
+	//TODO InfiniteLight相关
+
+	//获取这个点的自发光能量
+	Spectrum Le(const Scene& scene,const Vertex& v) const{
+		//先判断是否是光源
+		if(!IsLight()){
+			return Spectrum(0);
+		}
+		Vector3f w=v.p()-p();
+		if(w.LengthSquared()==0){
+			return Spectrum(0);
+		}
+		w=Normalize(w);
+		//TODO 这里也有InfiniteLight相关
+		const AreaLight* light=si.primitive->GetAreaLight();
+		return light->L(si,w);
 	}
 };
 
