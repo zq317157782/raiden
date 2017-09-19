0ce7522d2c162dd931c2295fdc34d8c35d9806c9
诸谦 <zq317157782@163.com>
2017-09-19 21:55:20 +0800

    修改部分microfacet.h文件中的注释，因为对于微平面有了新的理解

---
+++ src/core/microfacet.cpp
@@ -0,0 +1,1 @@
+#include "microfacet.h"
--- src/core/microfacet.h
+++ src/core/microfacet.h
@@ -16,19 +16,29 @@ protected:
 	const bool _sampleVisibleArea;
 public:
 	virtual ~MicrofacetDistribution(){};
-	//传入半角向量，返回相应方向的微平面总面积，或者说是法线分布
+
+	//NDF
+	//传入半角向量，返回相应方向的微平面总面积,或者说是法线分布
 	virtual Float D(const Vector3f &wh) const = 0;
+	
 	//Lambda是 不可见微平面面积 与 可见微平面面积 的比值
+	//或者是 可见微平面面积 与 总微平面面积 的比值
 	virtual Float Lambda(const Vector3f &w) const = 0;
-	//返回有多少比例的微平面可以被W方向看见，微平面的可见性假设和微平面的WH无关
+	
+	//Masking-Function
+	//返回有多少比例的法线为wh的微平面可以被w方向看见，微平面的可见性假设和微平面的wh无关
 	Float G1(const Vector3f &w) const {
 		//    if (Dot(w, wh) * CosTheta(w) < 0.0) return 0.0;
 		return 1.0 / (1.0 + Lambda(w));
 	}
+	
+	//Masking-Shadowing Function
 	//返回有多少比例的微平面可以同时被wo和wi方向同时看到
+	//这里使用的是考虑wo和wi的之间关联性的方法
 	Float G(const Vector3f &wo, const Vector3f &wi) const {
 		return 1.0 / (1.0 + Lambda(wo) + Lambda(wi));
 	}
+	
 	//virtual Vector3f Sample_wh(const Vector3f &wo, const Point2f &u) const = 0;
 	//Float Pdf(const Vector3f &wo, const Vector3f &wh) const;
 	//virtual std::string ToString() const = 0;
