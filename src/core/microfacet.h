7a88af26d5c1b69a06f635dd1268ffa114ce7c26
zhuqian <zhuqian@fantablade.com>
2017-09-20 13:08:31 +0800

    手动回撤前前个提交

--- src/core/microfacet.h
+++ src/core/microfacet.h
@@ -24,11 +24,11 @@ public:
 	
 	//Lambda是 不可见微平面面积 与 可见微平面面积 的比值
 	//或者是 可见微平面面积 与 总微平面面积 的比值
-	virtual Float Lambda(const Vector3f &w) const {return 0;};
+	virtual Float Lambda(const Vector3f &w) const = 0;
 	
 	//Masking-Function
 	//返回有多少比例的法线为wh的微平面可以被w方向看见，微平面的可见性假设和微平面的wh无关
-	virtual Float G1(const Vector3f &w) const {
+	Float G1(const Vector3f &w) const {
 		//    if (Dot(w, wh) * CosTheta(w) < 0.0) return 0.0;
 		return 1.0 / (1.0 + Lambda(w));
 	}
@@ -36,7 +36,7 @@ public:
 	//Masking-Shadowing Function
 	//返回有多少比例的微平面可以同时被wo和wi方向同时看到
 	//这里使用的是考虑wo和wi的之间关联性的方法
-	virtual Float G(const Vector3f &wo, const Vector3f &wi) const {
+	Float G(const Vector3f &wo, const Vector3f &wi) const {
 		return 1.0 / (1.0 + Lambda(wo) + Lambda(wi));
 	}
 	
