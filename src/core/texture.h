9cb093b809f391ba75c3b43a57b3d9922a8f6fb5
诸谦 <zq317157782@163.com>
2017-10-24 22:01:25 +0800

    加入cylindrical mapping和没有整合完的planar mapping

--- src/core/texture.h
+++ src/core/texture.h
@@ -40,6 +40,60 @@ public:
 	}
 };
 
+//把Object的坐标(x,y,z)投影到相应的圆柱体坐标(theta,z)上，然后再标准化到(s,t)空间中
+class CylindricalMapping2D:public TextureMapping2D{
+private:
+	const Transform _world2Tex;
+	Float _su; //dsdu
+	Float _sv; //dtdv
+	Float _du;
+	Float _dv;
+
+	//把(x,y,z)转换到(s,t)
+	Point2f PointToCylinderToST(const Point3f& pointW) const {
+		//首先转换到纹理空间
+		Point3f p = _world2Tex(pointW);
+		Vector3f v = Normalize(p - Point3f(0, 0, 0));
+		//计算球面坐标
+		Float theta = (Pi+std::atan2(v.y,v.x));
+		//标准化到(s,t)空间
+		return Point2f(theta*Inv2Pi,v.z);
+	}
+public:
+		CylindricalMapping2D(const Transform& w2t,Float su = 1.0, Float sv = 1.0, Float du = 0, Float dv = 0) :_world2Tex(w2t), _su(su), _sv(sv), _du(du), _dv(dv){
+	
+		}
+		virtual Point2f Map(const SurfaceInteraction &si, Vector2f *dstdx,
+			Vector2f *dstdy) const override {
+			
+			//计算st
+			Point2f st = PointToCylinderToST(si.p);
+			//计算近似偏导
+			Float delta = 0.01;//dX&dY
+			*dstdx = (PointToCylinderToST(si.p + delta*si.dpdx) - st) / delta;
+			*dstdy = (PointToCylinderToST(si.p + delta*si.dpdy) - st) / delta;
+			//考虑phi在0和2Pi之间的间断点情况
+			//从0到2PI
+			if ((*dstdx)[1] > 0.5) {
+				(*dstdx)[1] = (1 - (*dstdx)[1]);
+			}
+			//从2PI到0
+			else if ((*dstdx)[1] < -0.5) {
+				(*dstdx)[1] = -(1 + (*dstdx)[1]);
+			}
+			
+			//从0到2PI
+			if ((*dstdy)[1] > 0.5) {
+				(*dstdy)[1] = (1 - (*dstdy)[1]);
+			}
+			//从2PI到0
+			else if ((*dstdy)[1] < -0.5) {
+				(*dstdy)[1] = -(1 + (*dstdy)[1]);
+			}
+			return Point2f(_su * st.x + _du, _sv * st.y + _dv);
+		}
+};
+
 //把Object的坐标(x,y,z)投影到相应的Sphere坐标(theta,phi)上，然后再标准化到(s,t)空间中
 class SphericalMapping2D:public TextureMapping2D{
 private:
@@ -95,6 +149,31 @@ public:
 		return Point2f(_su * st.x + _du, _sv * st.y + _dv);
 	}
 };
+
+class PlanarMapping2D:public TextureMapping2D{
+private:
+	//组成平面坐标系的两根轴，可以非正交
+	Vector3f _vs;
+	Vector3f _vt;
+	
+	Float _ds;
+	Float _dt;
+public:
+	PlanarMapping2D(const Vector3f& vs,const Vector3f& vt,Float ds=0,Float dt=0):_vs(vs),_vt(vt),_ds(ds),_dt(dt){
+
+	}
+	
+	virtual Point2f Map(const SurfaceInteraction &si, Vector2f *dstdx,
+		Vector2f *dstdy) const override {
+			Vector3f vec(si.p);
+			//计算偏导在平面空间下的值
+			*dstdx=Vector2f(Dot(_vs,si.dpdx),Dot(_vt,si.dpdx));
+			*dstdy=Vector2f(Dot(_vs,si.dpdy),Dot(_vt,si.dpdy));
+			//转换到平面空间
+			return Point2f(Dot(_vs,vec)+_ds,Dot(_vt,vec)+_dt);
+		}
+	};
+
 
 //纹理接口
 template<typename T>
