4d637cb7237508be25368f06a6d9be208fe563b1
zhuqian <zhuqian@fantablade.com>
2017-10-24 15:18:25 +0800

    加入球面纹理坐标映射，支持scale和delta

--- src/core/texture.h
+++ src/core/texture.h
@@ -40,6 +40,62 @@ public:
 	}
 };
 
+//把Object的坐标(x,y,z)投影到相应的Sphere坐标(theta,phi)上，然后再标准化到(s,t)空间中
+class SphericalMapping2D:public TextureMapping2D{
+private:
+	const Transform _world2Tex;
+	Float _su; //dsdu
+	Float _sv; //dtdv
+	Float _du;
+	Float _dv;
+
+	//把(x,y,z)转换到(s,t)
+	Point2f PointToSphereToST(const Point3f& pointW) const {
+		//首先转换到纹理空间
+		Point3f p = _world2Tex(pointW);
+		Vector3f v = Normalize(p - Point3f(0, 0, 0));
+		//计算球面坐标
+		Float theta = SphericalTheta(v);
+		Float phi = SphericalPhi(v);
+		//标准化到(s,t)空间
+		return Point2f(theta*InvPi,phi*Inv2Pi);
+	}
+public:
+
+	SphericalMapping2D(const Transform& w2t, Float su = 1.0, Float sv = 1.0, Float du = 0, Float dv = 0) :_world2Tex(w2t), _su(su), _sv(sv), _du(du), _dv(dv) {
+
+	}
+	virtual Point2f Map(const SurfaceInteraction &si, Vector2f *dstdx,
+		Vector2f *dstdy) const override {
+		
+		//计算st
+		Point2f st = PointToSphereToST(si.p);
+		//计算近似偏导
+		Float delta = 0.1;//dX&dY
+		*dstdx = (PointToSphereToST(si.p + delta*si.dpdx) - st) / delta;
+		*dstdy = (PointToSphereToST(si.p + delta*si.dpdy) - st) / delta;
+		//考虑phi在0和2Pi之间的间断点情况
+		//从0到2PI
+		if ((*dstdx)[1] > 0.5) {
+			(*dstdx)[1] = (1 - (*dstdx)[1]);
+		}
+		//从2PI到0
+		else if ((*dstdx)[1] < -0.5) {
+			(*dstdx)[1] = -(1 + (*dstdx)[1]);
+		}
+		
+		//从0到2PI
+		if ((*dstdy)[1] > 0.5) {
+			(*dstdy)[1] = (1 - (*dstdy)[1]);
+		}
+		//从2PI到0
+		else if ((*dstdy)[1] < -0.5) {
+			(*dstdy)[1] = -(1 + (*dstdy)[1]);
+		}
+		return Point2f(_su * st.x + _du, _sv * st.y + _dv);
+	}
+};
+
 //纹理接口
 template<typename T>
 class Texture {
--- src/textures/checkerboard2d.cpp
+++ src/textures/checkerboard2d.cpp
@@ -35,6 +35,13 @@ Checkerboard2DTexture<Spectrum> *CreateCheckerboard2DSpectrumTexture(const Trans
 		//记住std::unique_ptr::reset用法!!!
 		mapping.reset(new UVMapping2D(uscale, vscale, udelta, vdelta));
 	}
+	else if (tp.FindString("mapping") == "spherical") {
+		Float uscale = tp.FindFloat("uscale", 1);
+		Float vscale = tp.FindFloat("vscale", 1);
+		Float udelta = tp.FindFloat("udelta", 0);
+		Float vdelta = tp.FindFloat("vdelta", 0);
+		mapping.reset(new SphericalMapping2D(Inverse(tex2world), uscale, vscale, udelta, vdelta));
+	}
 
 	//纹理
 	std::shared_ptr<Texture<Spectrum>> tex0 = tp.GetSpectrumTexture("tex1", 0);
