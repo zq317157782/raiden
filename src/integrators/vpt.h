ca1f366af1a6a04f9b1b151a49a01dc76772f176
zhuqian <zhuqian@fantablade.com>
2018-01-11 11:47:41 +0800

    为vpt添加etaScale的处理,直接光光源分布的处理以及去除不考虑镜面反射交点对能量的贡献的代码

--- src/integrators/vpt.h
+++ src/integrators/vpt.h
@@ -15,15 +15,24 @@
 #include "scene.h"
 #include "sampler.h"
 #include "reflection.h"
+#include "lightdistrib.h"
 
 class VolPathIntegrator : public SamplerIntegrator {
 private:
 	int _maxDepth; //路径的最大长度
 	Float _rrThreshold;//开始考虑俄罗斯罗盘的阈值
+	std::string _lightStrategy;
+	std::unique_ptr<LightDistribution> _lightDistribution;//光源的分布
 public:
 	VolPathIntegrator(int maxdepth, const std::shared_ptr<const Camera>& camera,
-		const std::shared_ptr<Sampler>& sampler, const Bound2i&pixelBound, Float rrThreshold = 1) :
-		SamplerIntegrator(camera, sampler, pixelBound), _maxDepth(maxdepth), _rrThreshold(rrThreshold) {
+		const std::shared_ptr<Sampler>& sampler, const Bound2i&pixelBound, Float rrThreshold = 1,const std::string& lightStrategy="uniform") :
+		SamplerIntegrator(camera, sampler, pixelBound), _maxDepth(maxdepth), _rrThreshold(rrThreshold),_lightStrategy(lightStrategy){
+	}
+
+	//VolPathIntegrator这里主要是为了处理相应的光源分布选择
+	//为啥是这两个参数是因为PBRT就用到这两个参数,哈哈哈哈
+	virtual void Preprocess(const Scene& scene,Sampler& sampler) override{
+		_lightDistribution=ComputeLightSampleDistribution(_lightStrategy,scene);
 	}
 
 	virtual Spectrum Li(const RayDifferential &r, const Scene &scene,
@@ -36,6 +45,16 @@ public:
 		bool isHit = false;
 		int bounces;//反射的次数
 		
+		//这里是PBRT第三版发布后加入的代码之一
+		//PBRT的解释也放在这了
+		// Added after book publication: etaScale tracks the accumulated effect
+		// of radiance scaling due to rays passing through refractive
+		// boundaries (see the derivation on p. 527 of the third edition). We
+		// track this value in order to remove it from beta when we apply
+		// Russian roulette; this is worthwhile, since it lets us sometimes
+		// avoid terminating refracted rays that are about to be refracted back
+		// out of a medium and thus have their beta value increased.
+		Float etaScale = 1;
 		
 		for (bounces = 0;; ++bounces) {
 			SurfaceInteraction ref; //和表面的交互点
@@ -61,8 +80,10 @@ public:
 				if (bounces >= _maxDepth) {
 					break;
 				}
-				//计算当前光源提供的直接光能量
-				L = L + beta*UniformSampleOneLight(mi, scene, arena, sampler, true);
+
+				//计算光源提供的直接光能量
+				const Distribution1D* distribution=_lightDistribution->Lookup(mi.p);
+				L = L + beta*UniformSampleOneLight(mi, scene, arena, sampler, true,distribution);
 				//采样新的方向
 				Vector3f wo =-ray.d;
 				Vector3f wi;
@@ -90,20 +111,28 @@ public:
 				}
 
 				ref.ComputeScatteringFunctions(ray, arena, true);
-				//这里考虑的其实是media边界的情况
-				//path 不考虑media,所以继续向下追踪
+				
+
 				if (!ref.bsdf) {
 					ray = ref.SpawnRay(ray.d);
 					--bounces;
 					continue;
 				}
 
+				//-----------------------------------------
+				//以下这部分代码是错误的，vpt的镜面反射需要考虑射线原点导镜面交点之间的中间介质对光源的影响产生的贡献
 				//判断bsdf中是否包含非specular成分，有的话，就要计算交点处的贡献
-				if (ref.bsdf->NumComponents(BxDFType(BSDF_ALL&~BSDF_SPECULAR))) {
-					Spectrum Ld = beta*UniformSampleOneLight(ref, scene, arena, sampler, true);
-					Assert(Ld.y() >= 0);
-					L += Ld;
-				}
+				// if (ref.bsdf->NumComponents(BxDFType(BSDF_ALL&~BSDF_SPECULAR))) {
+				// 	Spectrum Ld = beta*UniformSampleOneLight(ref, scene, arena, sampler, true);
+				// 	Assert(Ld.y() >= 0);
+				// 	L += Ld;
+				// }
+				//-----------------------------------------
+
+				//考虑中间介质和交点的影响
+				const Distribution1D* distribution=_lightDistribution->Lookup(ref.p);
+				Spectrum Ld = beta*UniformSampleOneLight(ref, scene, arena, sampler, true,distribution);
+				L += Ld;
 
 				//开始采样新的方向
 				Vector3f wo = -ray.d;
@@ -124,13 +153,20 @@ public:
 				}
 				beta = beta*(f*AbsDot(wi, ref.shading.n) / pdf);
 				Assert(beta.y() >= 0);
+
+				if((flag&BSDF_SPECULAR)&&((flag&BSDF_TRANSMISSION))){
+					//拿到ior的比例
+					Float eta= ref.bsdf->eta;
+					etaScale=etaScale*(Dot(wo,ref.n)>0)?(eta*eta):(1/(eta*eta));
+				}
+
 				//生成新射线
 				ray = ref.SpawnRay(wi);
 			}
 
-
-			if (beta.y()<_rrThreshold&&bounces>3) {
-				Float q = std::max(0.05, 1.0 - beta.MaxComponentValue());
+			Spectrum rrBeta=beta*etaScale;
+			if (rrBeta.y()<_rrThreshold&&bounces>3) {
+				Float q = std::max(0.05, 1.0 - rrBeta.MaxComponentValue());
 				if (sampler.Get1DSample() < q) {
 					break;
 				}
