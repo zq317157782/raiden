0cb69a96865c7ba3243b504863bbdd985bd31ad1
zhuqian <zhuqian@fantablade.com>
2018-01-19 16:59:09 +0800

    添加SIMD相关的宏定义

--- src/core/port.h
+++ src/core/port.h
@@ -18,11 +18,33 @@
 	#define COMPILER_MS
 #elif defined(__clang__)
 	#define COMPILER_CLANG 
-#endif
+#endif 
 
 #ifndef L1_CACHE_LINE_SIZE
-#define L1_CACHE_LINE_SIZE 64 //默认的cache line
-#endif
+	#define L1_CACHE_LINE_SIZE 64 //默认的cache line
+#endif //L1_CACHE_LINE_SIZE
+
+#ifdef IS_WIN
+	#ifdef COMPILER_MS
+		#ifdef _M_IX86_FP 
+			#ifdef __AVX__ 
+				#define SIMD_AVX
+			#elif __AVX2__
+				#define SIMD_AVX2
+			#else
+				#if _M_IX86_FP==0
+					#define SIMD_IA32
+				#elif  _M_IX86_FP == 1
+					#define SIMD_SSE	
+				#elif  _M_IX86_FP == 2
+					#define SIMD_SSE2
+				#endif
+			#endif
+		#endif //_M_IX86_FP
+	#endif // COMPILER_MS
+#endif // IS_WIN
+
+
 
 
 
