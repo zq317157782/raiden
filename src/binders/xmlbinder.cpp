ebf410a0fa7cba9011c0a927cd3f2aae5ec92c92
zhuqian <zhuqian@fantablade.com>
2018-11-15 10:49:23 +0800

    xml->point2f,point3f,vector2f,vector3f,normal3f

--- src/binders/xmlbinder.cpp
+++ src/binders/xmlbinder.cpp
@@ -35,6 +35,57 @@ ParamSet XMLBinder::PharseParamSet(const pugi::xml_node& root) const{
             set.AddBool(v_name, std::unique_ptr<bool[]>(new bool[1] {v}),1);
             LInfo<<"--->bool name:"<<v_name<<" value:"<<v;
         }
+        else if(strcmp(name,"rgb")==0){
+            const char* v_name=node.attribute("name").as_string();
+            float r=node.attribute("r").as_float();
+            float g=node.attribute("g").as_float();
+            float b=node.attribute("b").as_float();
+            set.AddRGBSpectrum(v_name,std::unique_ptr<float[]>(new float[3]{r,g,b}),3);
+            LInfo<<"--->rgb name:"<<v_name<<" value:["<<r<<","<<g<<","<<b<<"]";
+        }
+        else if(strcmp(name,"point2f")==0){
+            const char* v_name=node.attribute("name").as_string();
+            float x=node.attribute("x").as_float();
+            float y=node.attribute("y").as_float();
+            set.AddPoint2f(v_name,std::unique_ptr<Point2f[]>(new Point2f[1]{Point2f(x,y)}),1);
+            LInfo<<"--->point2f name:"<<v_name<<" value:["<<x<<","<<y<<"]";
+        }
+        else if(strcmp(name,"point3f")==0){
+            const char* v_name=node.attribute("name").as_string();
+            float x=node.attribute("x").as_float();
+            float y=node.attribute("y").as_float();
+            float z=node.attribute("z").as_float();
+            set.AddPoint3f(v_name,std::unique_ptr<Point3f[]>(new Point3f[1]{Point3f(x,y,z)}),1);
+            LInfo<<"--->point3f name:"<<v_name<<" value:["<<x<<","<<y<<","<<z<<"]";
+        }
+        else if(strcmp(name,"vector2f")==0){
+            const char* v_name=node.attribute("name").as_string();
+            float x=node.attribute("x").as_float();
+            float y=node.attribute("y").as_float();
+            set.AddVector2f(v_name,std::unique_ptr<Vector2f[]>(new Vector2f[1]{Vector2f(x,y)}),1);
+            LInfo<<"--->vector2f name:"<<v_name<<" value:["<<x<<","<<y<<"]";
+        }
+        else if(strcmp(name,"vector3f")==0){
+            const char* v_name=node.attribute("name").as_string();
+            float x=node.attribute("x").as_float();
+            float y=node.attribute("y").as_float();
+            float z=node.attribute("z").as_float();
+            set.AddVector3f(v_name,std::unique_ptr<Vector3f[]>(new Vector3f[1]{Vector3f(x,y,z)}),1);
+            LInfo<<"--->vector3f name:"<<v_name<<" value:["<<x<<","<<y<<","<<z<<"]";
+        }
+        else if(strcmp(name,"normal3f")==0){
+            const char* v_name=node.attribute("name").as_string();
+            float x=node.attribute("x").as_float();
+            float y=node.attribute("y").as_float();
+            float z=node.attribute("z").as_float();
+            set.AddNormal3f(v_name,std::unique_ptr<Normal3f[]>(new Normal3f[1]{Normal3f(x,y,z)}),1);
+            LInfo<<"--->normal3f name:"<<v_name<<" value:["<<x<<","<<y<<","<<z<<"]";
+        }
+        else if(strcmp(name,"integer_array")==0){
+            const char* v_name=node.attribute("name").as_string();
+            const char* v_value=node.child_value();
+            
+        }
         else if(strcmp(name,"string")==0){
             //字符串
         }
