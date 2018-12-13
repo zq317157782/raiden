e872720bf7e7e52db9eedfd789b0a66e1cc1e168
zhuqian <zhuqian@fantablade.com>
2018-12-13 17:42:35 +0800

    更新了lua快捷类型的实现

--- src/scripts/lua/raiden.lua
+++ src/scripts/lua/raiden.lua
@@ -1,25 +1,25 @@
 --定义了Lua端的各种类型
 
 function rgb(r,g,b)
-    return {type="rgb",value={r,g,b}}
+    return {"rgb",{r,g,b}}
 end
 
 function point3f(x,y,z)
-    return {type="point3f",value={x,y,z}}
+    return {"point3f",{x,y,z}}
 end
 
 function texture(name)
-    return {type="texture",value=name}
+    return {"texture",name}
 end
 
 function float_array( ... )
     temp = { ... }  
     arg = select("1",temp) 
-    return {type="float[]",value=arg} 
+    return {"float[]",arg} 
 end
 
 function int_array( ... )
     temp = { ... }  
     arg = select("1",temp) 
-    return {type="int[]",value=arg} 
+    return {"int[]",arg} 
 end
