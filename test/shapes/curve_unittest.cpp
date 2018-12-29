ca59806b7254090763df410eddcb27834584745e
zhuqian <zhuqian@fantablade.com>
2018-12-29 16:22:03 +0800

    为Curve添加相交相关的单元测试

--- test/shapes/curve_unittest.cpp
+++ test/shapes/curve_unittest.cpp
@@ -1,5 +1,6 @@
 #include "raiden.h"
 #include "gtest.h"
+#include "geometry.h"
 #include "shapes/curve.h"
 
 TEST(Curve,ObjectBound){
@@ -14,4 +15,27 @@ TEST(Curve,ObjectBound){
     EXPECT_FLOAT_EQ(bound.maxPoint.x,1.5);
     EXPECT_FLOAT_EQ(bound.maxPoint.y,0.5);
     EXPECT_FLOAT_EQ(bound.maxPoint.z,0.5);
+}
+
+
+TEST(Curve,Intersect){
+    Transform t;
+    auto common=std::make_shared<CurveCommon>(Point3f(0,0,0),Point3f(-0.2,0,0),Point3f(-0.8,0,0),Point3f(-1,0,0),1,1,CurveType::FLAT);
+    Curve curve(&t,&t,false,common,0,1);
+    Ray ray;
+    ray.o=Point3f(0,0,-1);
+    ray.d=Vector3f(0,0,2);
+    Float hit;
+    SurfaceInteraction is;
+    bool f=curve.Intersect(ray,&hit,&is);
+    EXPECT_TRUE(f);
+    EXPECT_FLOAT_EQ(hit,0.5);
+    EXPECT_EQ(is.n,Normal3f(0,0,-1));
+
+    ray.o=Point3f(0,-1,0);
+    ray.d=Vector3f(0,2,0);
+    f=curve.Intersect(ray,&hit,&is);
+    EXPECT_TRUE(f);
+    EXPECT_FLOAT_EQ(hit,0.5);
+    EXPECT_EQ(is.n,Normal3f(0,-1,0));
 }
