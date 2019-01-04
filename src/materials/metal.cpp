8eb1a2b432b14ed65d3143bf21a3298b96cba2b4
zhuqian <zhuqian@fantablade.com>
2019-01-04 11:12:27 +0800

    去除冗余的金属材质的打印信息

--- src/materials/metal.cpp
+++ src/materials/metal.cpp
@@ -4,6 +4,5 @@ MetalMaterial* CreateMetalMaterial(const TextureParams&mp){
     std::shared_ptr<Texture<Spectrum>>eta = mp.GetSpectrumTexture("eta", Spectrum(1.0f));
     std::shared_ptr<Texture<Spectrum>> absorb = mp.GetSpectrumTexture("absorb", Spectrum(1.0f));
     std::shared_ptr<Texture<Float>> roughness = mp.GetFloatTexture("roughness", 1.0f);
-	Debug("[CreateMetalMaterial]");
 	return new MetalMaterial(eta,absorb,roughness);
 }
