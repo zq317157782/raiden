625c95143283629867b9943f862df9f0e5998a7d
zhuqian <zhuqian@fantablade.com>
2019-01-09 18:10:04 +0800

    为hair材质新的参数

--- src/materials/hair.cpp
+++ src/materials/hair.cpp
@@ -1,6 +1,8 @@
 #include "hair.h"
 #include "paramset.h"
 HairMaterial* CreateHairMaterial(const TextureParams&mp){
-    std::shared_ptr<Texture<Float>> betaM=mp.GetFloatTexture("betam",Float(0.0));
-	return new HairMaterial(betaM);
+    auto eta=mp.GetFloatTexture("eta",Float(1.55));
+    auto betaM=mp.GetFloatTexture("betam",Float(1.0));
+    auto betaN=mp.GetFloatTexture("betan",Float(1.0));
+	return new HairMaterial(eta,betaM,betaN);
 }
--- src/materials/hair.h
+++ src/materials/hair.h
@@ -16,10 +16,12 @@
 #include "microfacet.h"
 class HairMaterial:public Material {
 private:
+    std::shared_ptr<Texture<Float>> _eta;
     std::shared_ptr<Texture<Float>> _betaM;
+    std::shared_ptr<Texture<Float>> _betaN;
 public:
 
-	HairMaterial(const std::shared_ptr<Texture<Float>>& betaM):_betaM(betaM){
+	HairMaterial(const std::shared_ptr<Texture<Float>>& eta,const std::shared_ptr<Texture<Float>>& betaM,const std::shared_ptr<Texture<Float>>& betaN):_eta(eta),_betaM(betaM),_betaN(betaN){
 	}
 
 	virtual void ComputeScatteringFunctions(SurfaceInteraction *si,
@@ -27,9 +29,14 @@ public:
 			bool allowMultipleLobes) const override{
 
         si->bsdf=ARENA_ALLOC(arena,BSDF)(*si);
+        
+        auto eta=_eta->Evaluate(*si);
 		//计算Mp的粗糙度
         auto betaM = _betaM->Evaluate(*si);
-        auto hair=ARENA_ALLOC(arena,HairBSDF(0,1.55,0,betaM,1,0));
+        //计算Np的粗糙度
+        auto betaN = _betaN->Evaluate(*si);
+        Float h=si->uv[1]*2-1;
+        auto hair=ARENA_ALLOC(arena,HairBSDF(h,eta,0,betaM,betaN,0));
         si->bsdf->Add(hair);
 	}
 	virtual ~HairMaterial(){};
