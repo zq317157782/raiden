bef6902ff667daddf6849a659b4b6c508d326edd
zhuqian <zhuqian@fantablade.com>
2019-01-11 10:49:52 +0800

    加入TrimmedLogisticPdf的标准化单元测试

---
+++ test/core/sampling_unittest.cpp
@@ -0,0 +1,16 @@
+#include "raiden.h"
+#include "gtest.h"
+#include "sampling.h"
+
+
+TEST(TrimmedLogisticPdf,Normalizition){
+    int count=30000;
+    RNG rng;
+    Float sum=0;
+    for(int i=0;i<count;++i){
+        Float x=rng.UniformFloat();
+        sum+=TrimmedLogisticPdf(x,1,0,1);
+    }
+    sum/=count;
+    EXPECT_GE(sum,0.99);
+}
