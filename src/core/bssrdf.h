d14feafa56cfd03e5b7031cee9daf208d6b3beee
zhuqian <zhuqian@fantablade.com>
2019-01-22 11:29:50 +0800

    复制两个pbrt的函数

--- src/core/bssrdf.h
+++ src/core/bssrdf.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "raiden.h"
 #include "geometry.h"
+
+//从PBRT复制的两个工具函数
+//使用多项式近似来近似菲涅尔的第一M和第二M
+//参数使用的是折射系数的倒数(和PBRT的实现有关)
+Float FresnelMoment1(Float invEta);
+Float FresnelMoment2(Float invEta);
+
 //双向次表面反射分布函数
 class BSSRDF{
 private:
