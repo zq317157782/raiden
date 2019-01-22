a1334af60a6cafb5ff19ac0b5a499126759499ad
zhuqian <zhuqian@fantablade.com>
2019-01-22 10:57:26 +0800

    添加BSSRDF抽象类

--- src/core/reflection.h
+++ src/core/reflection.h
@@ -910,4 +910,18 @@ class HairBSDF : public BxDF
 	// }
 	virtual Float Pdf(const Vector3f &wo, const Vector3f &wi) const override;
 };
+
+
+//双向次表面反射分布函数
+class BSSRDF{
+private:
+	const SurfaceInteraction &_po;//出射点和方向
+	Float _eta;//折射系数
+public:
+	BSSRDF(const SurfaceInteraction &po,Float eta):_po(po),_eta(eta){}
+	//S(po,wo,pi,wi)
+	//出射的微元radiance和入射的微元flux之比
+	virtual Spectrum S(const Point3f& pi,const Vector3f& wi) const=0;
+};
+
 #endif /* SRC_CORE_REFLECTION_H_ */
