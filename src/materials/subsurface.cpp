f750ab7b19accd2ebaee66ae6f8e964dd14e9dbf
zhuqian <zhuqian@fantablade.com>
2019-01-30 12:29:22 +0800

    为subsurface添加bsdf成分

--- src/materials/subsurface.cpp
+++ src/materials/subsurface.cpp
@@ -2,9 +2,16 @@
 #include "paramset.h"
 
 SubsurfaceMaterial* CreateSubsurfaceMaterial(const TextureParams&mp){
+    
+	std::shared_ptr<Texture<Spectrum>> r = mp.GetSpectrumTexture("Kr", Spectrum(1.0f));
+	std::shared_ptr<Texture<Spectrum>> t = mp.GetSpectrumTexture("Kt", Spectrum(1.0f));
+	std::shared_ptr<Texture<Float>> roughness = mp.GetFloatTexture("roughness", 0.0f);
+    
     std::shared_ptr<Texture<Spectrum>> sigmaS = mp.GetSpectrumTexture("sigmas", Spectrum(0.0f));
     std::shared_ptr<Texture<Spectrum>> sigmaA = mp.GetSpectrumTexture("sigmaa", Spectrum(0.0f));
-    Float eta=mp.FindFloat("eta",1);
+    Float eta=mp.FindFloat("eta",1.5f);
     Float g=mp.FindFloat("g",1);
-	return new SubsurfaceMaterial(sigmaS,sigmaA,eta,g);
+
+
+	return new SubsurfaceMaterial(r,t,roughness,sigmaS,sigmaA,eta,g);
 }
--- src/materials/subsurface.h
+++ src/materials/subsurface.h
@@ -17,18 +17,62 @@
 #include "bssrdf.h"
 class SubsurfaceMaterial:public Material {
 private:
-    BSSRDFTable _table;
+    BSSRDFTable _table; 
     std::shared_ptr<Texture<Spectrum>> _sigmaS;
     std::shared_ptr<Texture<Spectrum>> _sigmaA;
     Float _eta;
     Float _g;
+    std::shared_ptr<Texture<Spectrum>> _kr;
+	std::shared_ptr<Texture<Spectrum>> _kt;
+	std::shared_ptr<Texture<Float>> _roughness;
 public:
-	SubsurfaceMaterial(const std::shared_ptr<Texture<Spectrum>>& sigmaS,const std::shared_ptr<Texture<Spectrum>>& sigmaA,Float eta,Float g):_table(100,64),_sigmaS(sigmaS),_sigmaA(sigmaA),_eta(eta),_g(g){
+	SubsurfaceMaterial(const std::shared_ptr<Texture<Spectrum>>& kr, const std::shared_ptr<Texture<Spectrum>>& kt,const std::shared_ptr<Texture<Float>>& roughness,const std::shared_ptr<Texture<Spectrum>>& sigmaS,const std::shared_ptr<Texture<Spectrum>>& sigmaA,Float eta,Float g):_table(100,64),_kr(kr),_kt(kt),_roughness(roughness),_sigmaS(sigmaS),_sigmaA(sigmaA),_eta(eta),_g(g){
         ComputeBeamDiffusionBSSRDF(_g,_eta,&_table);
 	}
 	virtual void ComputeScatteringFunctions(SurfaceInteraction *si,
 			MemoryArena &arena, TransportMode mode,
 			bool allowMultipleLobes) const override{
+                //1.先为SurfaceInteraction分配bsdf
+		Spectrum R = _kr->Evaluate(*si);
+		Spectrum T = _kt->Evaluate(*si);
+        Float eta=_eta;
+		si->bsdf = ARENA_ALLOC(arena, BSDF)(*si,eta);
+		if(R.IsBlack()&&T.IsBlack()){
+			return;	
+		}
+
+		//2.判断是否是镜面反射或者折射
+		Float r = _roughness->Evaluate(*si);
+		bool isSpecular=(r==0);
+		//3.判断是否需要微平面分布
+		IsotropyGGXDistribution *ggx=(isSpecular==true)?nullptr:ARENA_ALLOC(arena, IsotropyGGXDistribution)(GGXRoughnessToAlpha(r));
+		
+		FresnelDielectric * fresnel=ARENA_ALLOC(arena, FresnelDielectric)(1.0,eta);
+
+		if (isSpecular&&allowMultipleLobes) {
+			si->bsdf->Add(ARENA_ALLOC(arena,FresnelSpecular)(R,T,1,eta,mode));
+		}
+		else {
+			if (!R.IsBlack()) {
+				if(isSpecular){
+					si->bsdf->Add(ARENA_ALLOC(arena, SpecularReflection)(R,fresnel));
+				}
+				else{
+					si->bsdf->Add(ARENA_ALLOC(arena,MicrofacetReflection)(R,ggx,fresnel));
+				}
+			}
+
+			if (!T.IsBlack()) {
+				if(isSpecular){
+					si->bsdf->Add(ARENA_ALLOC(arena, SpecularTransmission)(T,1,eta,mode));
+				}
+				else{
+					si->bsdf->Add(ARENA_ALLOC(arena,MicrofacetTransmission)(T,ggx,fresnel,1,eta,mode));
+				}
+			}
+		}
+
+
             Spectrum sigmaS=_sigmaS->Evaluate(*si);
             Spectrum sigmaA=_sigmaA->Evaluate(*si);
             //const SurfaceInteraction &po,Float eta,const Spectrum& sigmaS,const Spectrum& sigmaA,const BSSRDFTable& table
