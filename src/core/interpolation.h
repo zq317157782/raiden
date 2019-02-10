c0300c8a85fc951e4c22d03d1ea5f21933c49ba3
zhuqian <zq317157782@163.com>
2019-02-10 14:50:43 +0800

    补充注释

--- src/core/interpolation.h
+++ src/core/interpolation.h
@@ -31,4 +31,5 @@ Float IntegrateCatmullRom(int n, const Float *x, const Float *values,
 Float SampleCatmullRom(int n,const Float *x,const Float *f,const Float *F,Float u,Float* fval,Float* pdf);
 
 //采样二维的CatmullRom曲面上的一条曲线
+//alpha 在x1范围内的值
 Float SampleCatmullRom2D(int n1,int n2,const Float *x1,const Float *x2,const Float *f,const Float *cdf,Float alpha,Float u,Float* fval,Float* pdf);
