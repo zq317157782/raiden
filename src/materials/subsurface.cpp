20955b00bc045435138361fba39f248107fdfe19
zhuqian <zhuqian@fantablade.com>
2019-02-14 11:57:24 +0800

    修改默认g参数值

--- src/materials/subsurface.cpp
+++ src/materials/subsurface.cpp
@@ -10,7 +10,7 @@ SubsurfaceMaterial* CreateSubsurfaceMaterial(const TextureParams&mp){
     std::shared_ptr<Texture<Spectrum>> sigmaS = mp.GetSpectrumTexture("sigmas", Spectrum(0.0f));
     std::shared_ptr<Texture<Spectrum>> sigmaA = mp.GetSpectrumTexture("sigmaa", Spectrum(0.0f));
     Float eta=mp.FindFloat("eta",1.5f);
-    Float g=mp.FindFloat("g",1);
+    Float g=mp.FindFloat("g",0);
 
 
 	return new SubsurfaceMaterial(r,t,roughness,sigmaS,sigmaA,eta,g);
